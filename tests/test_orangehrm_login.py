0fb6dc1b70557da00ad6060cefc0d1cc6068a66c
Amroz <amrozkhan@gmail.com>
2026-03-10 08:48:31 +0000

    Week 3 Day 3: Test data management, JSON login data added

--- tests/test_orangehrm_login.py
+++ tests/test_orangehrm_login.py
@@ -1,16 +1,17 @@
 import re
+import json
 from playwright.sync_api import expect
 from pages.login_page import LoginPage
-from config.config import BASE_URL, USERNAME, PASSWORD
+
+with open("test_data/orangehrm_login_data.json") as f:
+    test_data = json.load(f)
+
+valid_username = test_data["valid_user"]["username"]
+valid_password = test_data["valid_user"]["password"]
 
 def test_orangehrm_login(page):
     login = LoginPage(page)
     login.open(BASE_URL)
-    login.login(USERNAME, PASSWORD)
-    #page.wait_for_timeout(timeout=10000)
-    expect(login.dashboard_button).to_be_visible()
-     #Wait for dashboard using URL pattern
-    page.wait_for_url(re.compile(r".*/dashboard.*"), timeout=15000)
-    expect(page).to_have_url(re.compile(".*dashboard.*"))
-    assert re.search(r"/dashboard", page.url, re.IGNORECASE), page.url
-#    assert "dashboard" in page.url.lower()
+    login.login(valid_username, valid_password)
+    expect(login.dashboard_button).to_be_visible(timeout=30000)
+    assert re.search(r"/dashboard", page.url, re.IGNORECASE), "Dashboard page did not load check url"
